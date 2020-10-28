730112cf86b7de71107722a9a0906537a953de7d
marie-hotchkiss <mtknowlton67@gmail.com>
2020-10-28 10:09:55 -0700

    passed dem 04.py

--- src/demonstration_04.py
+++ src/demonstration_04.py
@@ -5,11 +5,25 @@ Create a function that takes length and width and finds the perimeter of a
 rectangle.
 
 Examples:
-- find_perimeter(6, 7) ➞ 26
-- find_perimeter(20, 10) ➞ 60
-- find_perimeter(2, 9) ➞ 22
+print(find_perimeter(6, 7)) # ➞ 26
+print(find_perimeter(20, 10)) # ➞ 60
+print(find_perimeter(2, 9)) # ➞ 22
 """
 def find_perimeter(length, width):
     # Your code here
+    # set a perimeter to the value of the expression ( length + width) * 2
+    #perimeter = (length + width) * 2
+      return (length + width) * 2
+    # return perimeter to the caller
+  # return perimeter
+  
+  
+   # a function that takes length and width and finds the perimeter of a rectangle .
 
-    pass
+  
+  # return the value of the expression ( length + width) * 2 to the caller
+    
+
+print(find_perimeter(6, 7)) # ➞ 26
+print(find_perimeter(20, 10)) # ➞ 60
+print(find_perimeter(2, 9)) # ➞ 22
