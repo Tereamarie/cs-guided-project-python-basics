9b352e3ce10cbefd90f84e948e2d31b4c1a8d8dc
marie-hotchkiss <mtknowlton67@gmail.com>
2020-10-28 09:58:41 -0700

    passed dem 03

--- src/demonstration_03.py
+++ src/demonstration_03.py
@@ -4,10 +4,20 @@ Challenge #3:
 Create a function that takes a string and returns it as an integer.
 
 Examples:
-- string_int("6") ➞ 6
-- string_int("1000") ➞ 1000
-- string_int("12") ➞ 12
+print(string_int("6")) #➞ 6
+print(string_int("1000")) #➞ 1000
+print(string_int("12")) #➞ 12
 """
-def string_int(txt):
+#def string_int(txt):
     # Your code here
-    pass
+# set number to the int value of txt
+    # number = int(txt)
+    #return number
+
+def string_int(txt):
+    #your code here
+    return int(txt)
+
+print(string_int("6")) #➞ 6
+print(string_int("1000")) #➞ 1000
+print(string_int("12")) #➞ 12
