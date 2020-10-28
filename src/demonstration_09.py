a82b2ff2f483c1035365a04b3a529fb05adf6600
marie-hotchkiss <mtknowlton67@gmail.com>
2020-10-28 11:15:11 -0700

    passed dem 06.py

--- src/demonstration_09.py
+++ src/demonstration_09.py
@@ -5,9 +5,9 @@ Write a function that creates a dictionary with each (key, value) pair being
 the (lower case, upper case) versions of a letter, respectively.
 
 Examples:
-- mapping(["p", "s"]) ➞ { "p": "P", "s": "S" }
-- mapping(["a", "b", "c"]) ➞ { "a": "A", "b": "B", "c": "C" }
-- mapping(["a", "v", "y", "z"]) ➞ { "a": "A", "v": "V", "y": "Y", "z": "Z" }
+print(mapping(["p", "s"])) # ➞ { "p": "P", "s": "S" }
+print(mapping(["a", "b", "c"])) # ➞ { "a": "A", "b": "B", "c": "C" }
+print(mapping(["a", "v", "y", "z"])) # ➞ { "a": "A", "v": "V", "y": "Y", "z": "Z" }
 
 Notes:
 - All of the letters in the input list will always be lowercase.
@@ -15,3 +15,6 @@ Notes:
 def mapping(letters):
     # Your code here
     pass
+print(mapping(["p", "s"])) # ➞ { "p": "P", "s": "S" }
+print(mapping(["a", "b", "c"])) # ➞ { "a": "A", "b": "B", "c": "C" }
+print(mapping(["a", "v", "y", "z"])) # ➞ { "a": "A", "v": "V", "y": "Y", "z": "Z" }
