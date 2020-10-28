201807d3f2ad68f9467fecafcb514d8d33285685
marie-hotchkiss <mtknowlton67@gmail.com>
2020-10-28 09:41:32 -0700

    passed dem 02

--- src/demonstration_02.py
+++ src/demonstration_02.py
@@ -4,10 +4,25 @@ Challenge #2:
 Write a function that takes an integer `minutes` and converts it to seconds.
 
 Examples:
-- convert(5) ➞ 300
-- convert(3) ➞ 180
-- convert(2) ➞ 120
+-print(convert(5)) ➞ 300
+-print(convert(3)) ➞ 180
+-print(convert(2)) ➞ 120
 """
+# We can times minutes by 60 to get seconds
+# def convert(minutes):
+#     # set seconds to the value of the expression minutes * 60
+#     # Your code here
+#     seconds = minutes * 60
+#     # return seconds
+#     return seconds
 def convert(minutes):
+    #  return the value of the expression minutes * 60 to caller
+    return minutes * 60
     # Your code here
-    pass
+    
+    
+    
+
+print(convert(5))# ➞ 300
+print(convert(3)) #➞ 180
+print(convert(2))# ➞ 120
